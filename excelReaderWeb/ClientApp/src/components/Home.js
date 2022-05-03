fa8e0c8f6a24a7605cc3ad884363caed01f6a7ec
Fariz Izwan Ishak <Fariz_izwan_Ishak@Dell.com>
2022-05-04 05:23:52 +0800

    update home page

--- excelReaderWeb/ClientApp/src/components/Home.js
+++ excelReaderWeb/ClientApp/src/components/Home.js
@@ -1,26 +1,59 @@
 import React, { Component } from 'react';
 
 export class Home extends Component {
-  static displayName = Home.name;
+    static displayName = Home.name;
 
-  render () {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = { employee: [], loading: true };
+    }
+
+    componentDidMount() {
+        this.populateEmployeeData();
+    }
+
+    static renderEmployeeTable(employee) {
+        return (
+            <table className='table table-bordered' aria-labelledby="tabelLabel">
+                <thead>
+                    <tr>
+                        <th>Employee Number</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Employee Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {employee.map(employee =>
+                        <tr key={employee.EmployeeNumber} style={employee.EmployeeStatus === 'Regular' ? { color: 'green' } : { color: 'yellow' }}>
+                            <td>{employee.EmployeeNumber}</td>
+                            <td>{employee.FirstName}</td>
+                            <td>{employee.LastName}</td>
+                            <td>{employee.EmployeeStatus}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        );
+    }
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : Home.renderEmployeeTable(this.state.employee);
+
+        return (
+            <div>
+                <h1 id="tabelLabel" >Employee</h1>
+                {contents}
+            </div>
+        );
+    }
+
+    async populateEmployeeData() {
+        const response = await fetch('excel');
+        const data = await response.json();
+        console.log(`data`,data)
+        this.setState({ employee: data, loading: false });
+    }
 }
--- excelReaderWeb/Controllers/ExcelController.cs
+++ excelReaderWeb/Controllers/ExcelController.cs
@@ -1,33 +1,19 @@
-﻿using DocumentFormat.OpenXml;
-using DocumentFormat.OpenXml.Packaging;
-using DocumentFormat.OpenXml.Spreadsheet;
-using excelReaderWeb.Models;
-using excelReaderWeb.Services.Contract;
+﻿using excelReaderWeb.Services.Contract;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml;
 
 namespace excelReaderWeb.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class ExcelController : ControllerBase
     {
         private static readonly string fileName = @"C:\Users\fariz_izwan_ishak\Documents\Source Code\excelReaderWeb\sampleData.xlsx";
         private readonly IExcelService _excelService;
-        private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(IExcelService excelService, ILogger<WeatherForecastController> logger)
+        public ExcelController(IExcelService excelService)
         {
             _excelService = excelService;
-            _logger = logger;
         }
 
         [HttpGet]
